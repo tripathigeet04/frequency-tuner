06119e3ff1313daa64b60741fd04b22f50eb2e94
samue <redman.132@osu.edu>
2018-02-12 17:57:11 -0500

    Creating loop to generate multiple frequencies at a time

--- Algorithm/SoundGenration/generateSound.m
+++ Algorithm/SoundGenration/generateSound.m
@@ -1,8 +1,15 @@
 %this is a file to generate a sound file with a tone at a specific
 %frequency
 
+%loop to generate multiple frequencies at a time
+for i = 1 : 3
+
+%random int to produce frequency
+ran_num = rand * 1000
+
+
 %frequency of the tone we want (Hz)
-f = 440;
+f = ran_num;
 
 %amplitude of sound
 amp = 1;
@@ -19,13 +26,16 @@ snd=amp*cos(2*pi*f*t);
 
 
 
-for i = 1:15
+    for i = 1:15
     %generating random numbers to add in background frequencies
     snd = snd + .05 * randn(size(t));
-end
+    end
 
 
 sound(snd,fs);
+
+pause(4);
+end
 
 %% added part from meeting on 2-7-18
 

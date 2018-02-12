36e0f0523a6813ef4cb05f15f496c2b78dc8f7df
Alex Thieken <thieken9889@gmail.com>
2018-02-12 18:01:26 -0500

    continued working on FFT fuction

--- Algorithm/Spectrogram/FFT.m
+++ Algorithm/Spectrogram/FFT.m
@@ -1,4 +1,4 @@
-function [ FFT_output ] = FFT( vec,Fs,Plot )
+function [ FFT_complex,FFT_mag,freqs ] = FFT( vec,Fs,Plot )
 %FFT does a fft with the option to display the output in a graph
 %   Inputs:     vec --> n by 1 vector of time series data for the fft to be done on
 %               Fs  --> sample rate of vec
@@ -11,7 +11,16 @@ function [ FFT_output ] = FFT( vec,Fs,Plot )
     L = length(vec);
     
     %utilize fft fuction in matlab to produce the output vector
-    FFT_output = fft(vec)
-
+    FFT_complex = fft(vec);
+    
+    %compute the magnitude of each component from the fft
+    FFT_mag = abs
+    %create a vector of the frequencies for the axis
+    freqs =  Fs*(0:(L/2))/L;
+    
+    %plot if the varible plot is true
+    if(Plot)
+        plaot
+    end
 end
 

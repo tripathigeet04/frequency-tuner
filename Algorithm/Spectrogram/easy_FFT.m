742d0c37dc38aaa6b505f04ae47bb857c6a2b822
Alex Thieken <thieken9889@gmail.com>
2018-03-02 17:56:44 -0500

    Edited comments in easy_FFT.m

--- Algorithm/Spectrogram/easy_FFT.m
+++ Algorithm/Spectrogram/easy_FFT.m
@@ -3,6 +3,15 @@ function [ FFT_complex,FFT_mag,freqs ] = FFT( vec,Fs,Plot )
 %   Inputs:     vec --> n by 1 vector of time series data for the fft to be done on
 %               Fs  --> sample rate of vec
 %               Plot--> boolean for whether to generate a plot or not
+%
+%   Outputs:    FFT_complex     --> The full complex outputs of the matlab
+%                                   fft function
+%               FFT_mag         --> Only the magnitude information of the
+%                                   postive frequency
+%               freqs           --> Vectors whose indices contain the
+%                                   frequencies that correspond to the
+%                                   FFT_mag and the first half of
+%                                   FFT_complex
     
     %sampling period
     Ts = 1/Fs;
@@ -15,13 +24,14 @@ function [ FFT_complex,FFT_mag,freqs ] = FFT( vec,Fs,Plot )
     FFT_complex = fft(vec);
     
     %compute the magnitude of each component from the fft
-    %this output includes negative and positive frequencies
+    %this output includes only positive frequencies and is normalized by
+    %the length of the the sound vector
     FFT_mag = abs(FFT_complex(1:(L/2)+1)/(L/2));
     
     %create a vector of the frequencies for the axis
     freqs =  Fs*(0:(L/2))/L;
     
-    %plot if the varible plot is true
+    %plot only the positive frequency magnitudes if the varible plot is true
     if(Plot)
         figure;
         plot(freqs,FFT_mag);

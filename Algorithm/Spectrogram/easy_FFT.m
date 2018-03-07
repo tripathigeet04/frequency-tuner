68c3826f7b9cc8de82008bc9dc7a875ca3548e90
Alex Thieken <thieken9889@gmail.com>
2018-03-06 20:10:43 -0500

    changed easy_FFT function to output and graph in dB

--- Algorithm/Spectrogram/easy_FFT.m
+++ Algorithm/Spectrogram/easy_FFT.m
@@ -1,4 +1,4 @@
-function [ FFT_complex,FFT_mag,freqs ] = FFT( vec,Fs,Plot )
+function [ FFT_complex,FFT_mag,FFT_mag_dB,freqs ] = easy_FFT( vec,Fs,Plot )
 %FFT does a fft with the option to display the output in a graph
 %   Inputs:     vec --> n by 1 vector of time series data for the fft to be done on
 %               Fs  --> sample rate of vec
@@ -8,6 +8,7 @@ function [ FFT_complex,FFT_mag,freqs ] = FFT( vec,Fs,Plot )
 %                                   fft function
 %               FFT_mag         --> Only the magnitude information of the
 %                                   postive frequency
+%               FFT_mag_dB      --> FFT_mag in decibels
 %               freqs           --> Vectors whose indices contain the
 %                                   frequencies that correspond to the
 %                                   FFT_mag and the first half of
@@ -28,15 +29,27 @@ function [ FFT_complex,FFT_mag,freqs ] = FFT( vec,Fs,Plot )
     %the length of the the sound vector
     FFT_mag = abs(FFT_complex(1:(L/2)+1)/(L/2));
     
+    %calculate FFT_mag in decibels
+    FFT_mag_dB = 20*log10(FFT_mag);
+    
     %create a vector of the frequencies for the axis
     freqs =  Fs*(0:(L/2))/L;
     
     %plot only the positive frequency magnitudes if the varible plot is true
     if(Plot)
+        %plot the normalized magnitudes
         figure;
         plot(freqs,FFT_mag);
+        title('DFT of Timeseries Data');
         xlabel('Frequency (Hz)');
         ylabel('Magnitude (normalized)');
+        
+        %plot the manitudes in dB
+        figure;
+        plot(freqs,FFT_mag_dB);
+        title('DFT of Timeseries Data in dB');
+        xlabel('Frequency (Hz)');
+        ylabel('Magnitude (dB)');
     end
 end
 
